150e5282d1fa99502ffebb91084ff2242873d1b1
Oscar <pamo@tuta.com>
2024-03-07 23:28:13 +1100

    I love javascript

--- installtw.sh
+++ installtw.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 
 if ! test -f "tailwindcss" ; then
-    curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/download/v3.4.1/tailwindcss-linux-x64
+    curl -LO https://github.com/tailwindlabs/tailwindcss/releases/download/v3.4.1/tailwindcss-linux-x64
     chmod +x tailwindcss-linux-x64
     mv tailwindcss-linux-x64 tailwindcss
 fi
