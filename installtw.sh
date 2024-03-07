28965ffd4a3150edfe485ed62fbd3527d1fbdefe
Oscar <pamo@tuta.com>
2024-03-07 22:49:02 +1100

    getting there

---
+++ installtw.sh
@@ -0,0 +1,7 @@
+
+if ! test -f "tailwindcss" ; then
+    curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/download/v3.4.1/tailwindcss-linux-x64
+    chmod +x tailwindcss-linux-x64
+    mv tailwindcss-linux-x64 tailwindcss
+fi
+./tailwindcss -i css/input.css -o css/output.css --watch
