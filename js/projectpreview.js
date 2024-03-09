e50c6f13aab95046e57635e2fa52fb3e66572d7a
Oscar <pamo@tuta.com>
2024-03-09 11:26:19 +1100

    ok thanks dolphin mixtral that looks really good cheers

--- js/projectpreview.js
+++ js/projectpreview.js
@@ -8,17 +8,12 @@ class ProjectPreview extends HTMLElement {
     let content = this.getAttribute('content');
     let image = this.getAttribute('image');
     this.innerHTML = `
-      <div class="m-3 mt-12 w-[100%]">
-        <div class="relative min-h-40 rounded-lg">
-          <div class="">
-            <div class="z-10 m-4  bg-white bg-opacity-80 min-h-40 relative">
-              <h3 class="text-2xl relative font-comfortaa z-10">` + title + `</h3>
-              <p class="text-justify z-10">` + content + `</p>
-            </div>
-          </div>
-          <div class="overflow-hidden w-full h-full absolute top-0">
-            <img class="absolute top-0 -z-0" src="` + image + `" />
-          </div>
+      <div class="mt-10 border-2 border-zinc-100 shadow hover:shadow-lg rounded-md overflow-hidden transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer">
+
+        <img src="images/` + image + `" alt="`+title+`" class="w-full h-48 object-cover">
+        <div class="p-4">
+          <h3 class="text-2xl font-bold text-gray-900 mb-2">`+title+`</h3>
+          <p class="text-base text-gray-500">`+content+`</p>
         </div>
       </div>
     `;
